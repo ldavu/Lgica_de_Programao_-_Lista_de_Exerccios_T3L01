727370d9c0d84534edbc015df43dd208cf441241
Davu <41762352+ldavu@users.noreply.github.com>
2021-10-19 20:41:58 -0300

    Update ex-004.py

--- ex-004.py
+++ ex-004.py
@@ -1,23 +1,19 @@
 pontos = 100
 pontos_jogador = 0
 arremessos = 0
-metro5 = 20 #>
-metro2 = 10 #>
-metro = 5   #<=
-
 
 while(pontos_jogador < 100):
     print('Qual metro você lançou até a cesta?')
     resposta = int(input())
 
     if(resposta > 5):
-        pontos_jogador = pontos_jogador + metro5
+        pontos_jogador += 20
         arremessos += 1
     elif(resposta > 2):
-        pontos_jogador = pontos_jogador + metro2
+        pontos_jogador += 10
         arremessos += 1
     elif(resposta <= 2):
-        pontos_jogador = pontos_jogador + metro
+        pontos_jogador += 5
         arremessos += 1
     
     if(pontos_jogador >= pontos):
