df60f0d8b5806428a55c8c2a7a301076ef385251
Davu <41762352+ldavu@users.noreply.github.com>
2021-10-19 19:53:02 -0300

    Update ex-001.py

--- ex-001.py
+++ ex-001.py
@@ -1,27 +1,20 @@
 import random
+from time import sleep
 
 vida_jogador = 100
-vida_boss = 100
 
-tirar_vida = random.randint(14,25)
-tirar_vida_boss = random.randint(12,15)
 
-print("digite 'bater' para matar o boss")
-resposta_bater = str(input)
 
 while (vida_jogador > 0):
-
-    resposta_bater = str(input())
     
-    if (resposta_bater == "bater"):
-        vida_boss = vida_boss - tirar_vida
-        vida_jogador = vida_jogador - tirar_vida_boss
-        print(f"Você tirou {tirar_vida}")
-        print(f"Vida do boss {vida_boss}")
-        print(f"Sua vida {vida_jogador}")
+    ataque = random.randint(15,24)
+
+    vida_jogador = vida_jogador - ataque
+    sleep(1)
+
+    print(f"o inimigo tirou {ataque}")
+    print(f"Sua vida {vida_jogador}")
 
     if (vida_jogador <= 0):
-        print("Você morreu...")
+        print("Você perdeu...")
 
-    if (vida_boss <= 0):
-        print("Parabéns jogador você matou o boss!")
