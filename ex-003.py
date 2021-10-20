9fa9e26a030b3bc39eba6237514ce19582e75f3e
Davu <41762352+ldavu@users.noreply.github.com>
2021-10-19 21:24:37 -0300

    Update ex-003.py

--- ex-003.py
+++ ex-003.py
@@ -1,22 +1,22 @@
-sair_programa = 'sim'
+sair_programa = ""
 
 entregas = 0
 retirar = 0
 
-while (sair_programa != "sim"):
+while (sair_programa != "não"):
     print("Olá entregador! Você o que você vai fazer? entregar | retirar | sair")
-    resposta = str(input())
+    resposta = input()
 
-    if(resposta == "entregar"):
+    if(resposta == "retirar"):
         retirar += 1
     elif(resposta == "entregar"):
         entregas += 1
 
-
-    print(f"Você entregou {entregas}")
-    print(f"Você retirou {retirar}")
-    
     print("Deseja sair do programa?")
     resposta_sair = str(input())
     if(resposta_sair == "sim"):
         print("você saiu do programa!")
+        print("-"*15)
+        if(entregas > 1):print(f"Você fez {entregas} entregas")
+        else:print(f"Você fez {entregas} entrega")
+        print(f"Você retirou {retirar}")
